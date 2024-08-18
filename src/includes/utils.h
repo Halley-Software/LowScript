95fa7b56096494d2caf12780e9705a1e50833432
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-08-19 00:08:33 +0200

    feat: added function to convert single chars to 'std::string' objects
    Also the macro 'DEBUG' has been refactored and now is indented

--- src/includes/utils.h
+++ src/includes/utils.h
@@ -1,19 +1,24 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <string>
+
 // Determines the length of an array
 #define arr_len(array) sizeof(array) / sizeof(array[0])
 
-#ifndef DEBUG
-#define dbg(str)
+/**
+ * Returns a new string containing the 'c' char as content
+ */
+extern std::string char_to_str(const char& c);
 
-#define curtt(str)
+#ifdef DEBUG
+  #include <iostream>
+
+  #define dbg(str) std::cout << str << std::endl
+  #define curtt(str) std::cout << "Current tok -> " + std::string(1, str) << std::endl
 #else
-#include <iostream>
-#include <string>
-#define dbg(str) std::cout << str << std::endl
-
-#define curtt(str) std::cout << "Current tok -> " + std::string { str } << std::endl
+  #define dbg(str)
+  #define curtt(str)
 #endif // DEBUG
 
 #endif // UTILS_H

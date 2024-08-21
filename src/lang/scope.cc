a24ac2724db1769fc455f0c1f77772ddeb2b52fe
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-08-21 22:17:51 +0200

    feat: implemented methods for 'scope.h'

--- src/lang/scope.cc
+++ src/lang/scope.cc
@@ -1,37 +1,56 @@
 #include <format>
-#include <map>
+#include <memory>
 #include <string>
+#include <utility>
 
 #include "lang/scope.h"
+
+// The inclusion of commons helps the compiler to find the full declaration of `Entity` class
 #include "lang/commons.h"
 
 namespace lowscript {
 namespace lang {
 
-//ReferenceException::ReferenceException(std::string message): reason(message) {};
+using Nameable = Entity;
 
-Scope::Scope(Scope* parent, std::string owner):
-    parent_scope(parent),
-    belongs_to(owner) {
-    variables = std::map<std::string, Entity>();
-}
+Scope::Scope(std::unique_ptr<Scope> parent, std::string owner):
+    parent_scope(std::move(parent)),
+    belongs_to(owner),
+    variables(std::map<std::string, Nameable>()) {}
 
-bool Scope::has(std::string name) const {
-    return variables.find(name) != variables.end();
-}
-
-void Scope::add_entry(std::string name, Entity value) {
+void Scope::add_entry(std::string name, Nameable value) {
     if (has(name))
         throw std::format("Entry '{}' already exists", name);
 
-    variables.insert(std::pair<std::string, Entity>(name, value));
+    variables.insert(std::pair<std::string, Nameable>(name, value));
 }
 
-Entity Scope::get(std::string name) {
-    if (!has(name))
-        throw std::format("Entry '{}' does not exists", name);
+const Nameable* Scope::search(std::string name) const {
+    std::map<std::string, Nameable>::const_iterator entry = variables.find(name);
 
-    return (variables.find(name))->second;
+    if (entry != variables.end())
+        return &entry->second;
+    else if (parent_scope != nullptr)
+        return search(name);
+    else
+        return nullptr;
+}
+
+bool Scope::has(std::string name) const {
+    const Nameable* entry = search(name);
+
+    return entry == nullptr ? false : true;
+}
+
+const Nameable* Scope::get(std::string name) const {
+    return search(name);
+}
+
+// ----- ReferenceException what redefinition -----
+ReferenceException::ReferenceException(const char* message): reason(message) {}
+
+const char* ReferenceException::what() const noexcept {
+    return reason;
 }
 
 } // lang
